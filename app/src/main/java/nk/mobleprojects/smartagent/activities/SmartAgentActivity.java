fd21c758602b11da489659a8356b7a7f4a278636
VisarapuPrudhviRaj <prudhvirajvisarapu@gmail.com>
2019-08-12 17:19:56 +0530

    Update

--- app/src/main/java/nk/mobleprojects/smartagent/activities/SmartAgentActivity.java
+++ app/src/main/java/nk/mobleprojects/smartagent/activities/SmartAgentActivity.java
@@ -1,6 +1,7 @@
 package nk.mobleprojects.smartagent.activities;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -17,6 +18,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -56,6 +58,21 @@ public class SmartAgentActivity extends BaseActivity {
         }
     }
 
+    private String checkFileExist(String fileName) {
+
+        File sdcard = new File(Environment.getExternalStorageDirectory() + "/" + Helper.FOLDER_NAME);
+        if (!sdcard.exists()) {
+            sdcard.mkdir();
+        }
+        File file = new File(sdcard, fileName);
+        if (file.exists()) {
+
+            return file.getAbsolutePath();
+
+        } else {
+            return "";
+        }
+    }
 
     private void serverHit() {
         showProgressDialog("Please Wait ...");
@@ -74,10 +91,22 @@ public class SmartAgentActivity extends BaseActivity {
                                 JSONObject result = jsonArray.getJSONObject(i);
 
                                 if (dbHelper.getCountByValue(DBTables.SmartProject.TABLE_NAME, DBTables.SmartProject.id, result.getString("id")) == 0) {
-                                    dbHelper.insertintoTable(DBTables.SmartProject.TABLE_NAME,
-                                            DBTables.SmartProject.cols, new String[]{result.getString("id"),
-                                                    result.getString("name"), result.getString("type"),
-                                                    result.getString("sizeInBytes"), result.getString("cdn_path"), "", "0"});
+                                    //file_name: SD check
+                                    String file_path = checkFileExist(result.getString("name"));
+                                    if (file_path.equals("")) {
+
+                                        dbHelper.insertintoTable(DBTables.SmartProject.TABLE_NAME,
+                                                DBTables.SmartProject.cols, new String[]{result.getString("id"),
+                                                        result.getString("name"), result.getString("type"),
+                                                        result.getString("sizeInBytes"), result.getString("cdn_path"), "", "0"});
+                                    } else {
+                                        dbHelper.insertintoTable(DBTables.SmartProject.TABLE_NAME,
+                                                DBTables.SmartProject.cols, new String[]{result.getString("id"),
+                                                        result.getString("name"), result.getString("type"),
+                                                        result.getString("sizeInBytes"), result.getString("cdn_path"),file_path , "1"});
+                                    }
+
+
                                 }
 
 
